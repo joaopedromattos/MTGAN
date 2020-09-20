581dd0391b3e6cb67b90c0ef34274cbc60cf7dfc
João Pedro Mattos <joao_pedro_mattos@hotmail.com>
2020-09-20 19:59:06 -0300

    DCGAN Baseline Complete!
    This commit adds a DCGAN baseline that's capable of succesfully overfitting a single batch of our MTG Dataset! :rocket:

---
+++ magic_card_generator/baseline/dcgan/README.md
@@ -0,0 +1,19 @@
+# DCGAN - Baseline
+
+This folder contains a simple implementation of DCGAN, from the [official PyTorch documentation](https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html).
+
+The main purpose of this baseline is to establish a feasible starting point to our group. Thus, we chose DCGAN, an easy and well known GAN architecture.
+
+
+## Directory file structure
+```
+.
+└── dcgan
+    ├── model.py -> Model class definition
+    ├── README.md
+    ├── test.py -> Model test unit
+    ├── train.py -> Training pipeline instantiation
+    └── utils.py -> Utilitary functions used while training
+```
+
+**Warning** : This code is not functional __(yet)__.
---
+++ magic_card_generator/baseline/dcgan/utils.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+#%matplotlib inline
+import argparse
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from IPython.display import HTML
+
+# custom weights initialization called on netG and netD
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
